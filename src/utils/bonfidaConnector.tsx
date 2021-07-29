a051de7dafa3c1125ffbd15af8331445f16c12a7
Ateeq <ateeq.rehmani@ideofuzion.com>
2021-07-29 17:59:45 +0500

    change

--- src/utils/bonfidaConnector.tsx
+++ src/utils/bonfidaConnector.tsx
@@ -1,7 +1,7 @@
 import { BonfidaTrade } from './types';
 
 export default class BonfidaApi {
-  static URL: string = 'https://api.dexlab.space/v1/trade-history/';
+  static URL: string = 'https://serum-api.bonfida.com/';
 
   static async get(path: string) {
     try {
